ee8be3406142a6162611cac2cc0abc89c74b5f7e
mWtbg <165910605+mWtbg@users.noreply.github.com>
2024-04-03 15:28:11 +0200

    Change location of components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import ChuckQuote from "./components/chuckQuote";
+import ChuckQuote from "@/components/chuckQuote";
 
 export default function Home() {
   /*async function handleOnHelloClick(){
---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export default function Headere() {
+
+    return(
+        <header>
+            <h1> This is a Header</h1>
+        </header>
+    )
+}
