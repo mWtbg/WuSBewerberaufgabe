14c99163c6f21128b5cf9de9f86cad2560c427c5
mWtbg <165910605+mWtbg@users.noreply.github.com>
2024-04-09 13:22:42 +0200

    add chuck api category, hover effect & hide quotes

--- components/chuckQuote.tsx
+++ components/chuckQuote.tsx
@@ -9,11 +9,31 @@ interface ChuckResponse {
   value: string;
 }
 
+function Quotes({quote, isfirstQuote}:{quote: string|undefined, isfirstQuote:boolean}) {
+  if (!quote) {
+    return null;
+  }
+
+  return (
+    <svg
+      className={'text-end w-8 h-8 text-zinc-900 mb-1 '+ ( isfirstQuote ?  "": "self-end")}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 18 14"
+    >
+      <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+    </svg>
+  );
+}
+
 export default function ChuckQuote() {
   const [quote, setQuote] = React.useState<string>();
 
   async function handleOnClick() {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const response = await fetch(
+      "https://api.chucknorris.io/jokes/random?category=dev"
+    );
     const chuckResponse = (await response.json()) as ChuckResponse;
     setQuote(chuckResponse.value);
   }
@@ -22,33 +42,17 @@ export default function ChuckQuote() {
     <div className="grow h-full text-lg flex gap-50 flex-wrap flex-col items-center">
       <div className="grow-[2]"></div>
       <blockquote className="flex flex-col">
-        <svg
-          className="text-end w-8 h-8 text-zinc-900 mb-1"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 18 14"
-        >
-          <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-        </svg>
+        <Quotes quote={quote} isfirstQuote={true} />
         <p className="px-9 break-words w-full font-medium text-2xl">{quote}</p>
-        <svg
-          className="text-end w-8 h-8 text-zinc-900 self-end"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 18 14"
-        >
-          <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-        </svg>
+        <Quotes quote={quote} isfirstQuote={false} />
       </blockquote>
       <div className="grow"></div>
-      <div className="bg-zinc-700 p-px shadow-2xl">
+      <div className="bg-zinc-700 p-px shadow-2xl hover:bg-zinc-500 ">
         <button
           onClick={handleOnClick}
-          className=" p-3 border-2 border-white bg-zinc-700 text-white font-bold"
+          className=" p-3 border-2 border-white bg-zinc-700 text-white font-bold hover:bg-zinc-500 "
         >
-          Chuck Norris Quote
+          Get a Chuck Norris Quote
         </button>
       </div>
       <div className="h-2/5 shrink"></div>
